d2ae0d4f7984b34891b4ceba0a0c59f0b833997d
soumyadip007 <soumyadip.note@gmail.com>
2019-08-23 22:02:51 +0530

    Main.js bug rem

--- js/main.js
+++ js/main.js
@@ -115,12 +115,11 @@
 		// clearTimeout(timer);
 		$this.addClass('show');
 		$this.find('> a').attr('aria-expanded', true);
-		// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
+		
 		$this.find('.dropdown-menu').addClass('show');
 	}, function(){
 		var $this = $(this);
-			// timer;
-		// timer = setTimeout(function(){
+			
 			$this.removeClass('show');
 			$this.find('> a').attr('aria-expanded', false);
 			// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
