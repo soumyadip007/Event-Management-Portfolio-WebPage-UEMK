9f08ff3603470edf1f303452f42f7dbd5daaf4d4
soumyadip007 <soumyadip.note@gmail.com>
2019-08-23 20:29:19 +0530

    Time config

--- js/main.js
+++ js/main.js
@@ -273,7 +273,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("1 September 2019 9:56:00 GMT+01:00");			
+		var endTime = new Date("1 September 2019 9:00:00 GMT+01:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
